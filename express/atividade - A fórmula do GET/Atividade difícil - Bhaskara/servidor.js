a5ac7cb7f0ea36c70f87ed23343d284a5368e736
Davi Asaf <daviasaf16@gmail.com>
2025-06-08 12:47:52 -0300

    Atividade mais difícil finalizada

--- express/atividade - A fórmula do GET/Atividade difícil - Bhaskara/servidor.js
+++ express/atividade - A fórmula do GET/Atividade difícil - Bhaskara/servidor.js
@@ -10,9 +10,33 @@ app.get('/', (req, res) => {
     res.render('conta')
 })
 app.get('/resultado', (req, res) => {
-    let n1 = Number(req.query.num1)
-    let total = n1**2
-    res.render('resultado', {total})
+
+    let a = Number(req.query.a)
+    let b = Number(req.query.b)
+    let c = Number(req.query.c)
+    let delta = (b**2) - (4*a*c)
+    let mensagem, x1, x2;
+    if(delta<0){
+        mensagem = "Não há raízes reais"
+    }
+    else if(delta===0){
+        mensagem = "Existe apenas uma raiz real."
+        x1 = (-b + Math.sqrt(delta))/(2*a)
+        x2 = x1
+    }
+    else{
+        mensagem = "existem duas raízes reais."
+        x1 = (-b + Math.sqrt(delta))/(2*a)
+        x2 = (-b - Math.sqrt(delta))/(2*a)
+    }
+    res.render('resultado', {
+        a:a,
+        b:b,
+        c:c,
+        mensagem:mensagem,
+        x1:x1,
+        x2:x2
+    })
 })
 app.listen(port, () => {
     console.log(`Servidor rodando na porta http://localhost:${port}`)
