d6e46f8afd1d7fac2dacc4c46ea4168fd03a0781
Davi Asaf <daviasaf16@gmail.com>
2025-06-08 10:47:15 -0300

    Adicinando novas pastas para as 3 atividades

---
+++ express/atividade - A fórmula do GET/Atividade útil - juros simples/servidor.js
@@ -0,0 +1,19 @@
+//Servidor
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('conta')
+})
+app.get('/resultado', (req, res) => {
+    let n1 = Number(req.query.num1)
+    let total = n1**2
+    res.render('resultado', {total})
+})
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}`)
+})
